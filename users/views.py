4adf4b4c15bda0692ada8e3686bfba9fa25e627a
mahdibagheri <bagheri.almas@gmail.com>
2023-08-24 14:29:46 +0330

    Implemented UserRegisterView in views.py for user registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import CustomUserCreationForm
+
+
+class UserRegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = 'login'
+    template_name = 'registration/register.html'
