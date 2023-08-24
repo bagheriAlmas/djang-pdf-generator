01d8683fb48c801016df0e41dd498678fab694bf
mahdibagheri <bagheri.almas@gmail.com>
2023-08-24 14:09:15 +0330

    Added CustomUserCreationForm and CustomUserChangeForm to forms.py

---
+++ users/forms.py
@@ -0,0 +1,15 @@
+from django.forms import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserChangeForm):
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = ['username', 'email', 'age', 'phone', 'gender']
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm):
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
