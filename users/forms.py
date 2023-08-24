81b932959189cf59db4f99bb5388536037193ece
mahdibagheri <bagheri.almas@gmail.com>
2023-08-24 16:51:04 +0330

    Fixed mistake: Changed UserChangeForm to UserCreationForm in CustomUserCreationForm

--- users/forms.py
+++ users/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 
-class CustomUserCreationForm(UserChangeForm):
+class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
         fields = ['username', 'email', 'age', 'phone', 'gender']
