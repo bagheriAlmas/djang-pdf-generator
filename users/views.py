fdca327d9f04db02d94617eef45983f9e89053d1
mahdibagheri <bagheri.almas@gmail.com>
2023-08-24 19:06:24 +0330

    add form_valid function for UserRegisterView to check form validation

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -9,3 +10,7 @@ class UserRegisterView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/register.html'
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Registration failed. Please correct the errors.')
+        return super().form_invalid(form)
