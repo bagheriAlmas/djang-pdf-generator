3db6387b6338d862619850f83de70b2b2a7f96a0
mahdibagheri <bagheri.almas@gmail.com>
2023-08-24 16:43:31 +0330

    Add LOGIN_REDIRECT_URL and LOGOUT_REDIRECT_URL in settings.py

--- pdf_generator/settings.py
+++ pdf_generator/settings.py
@@ -104,4 +104,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 AUTH_USER_MODEL = 'users.CustomUser'
+LOGIN_REDIRECT_URL = '../../../'
+LOGOUT_REDIRECT_URL = 'registration/login'
