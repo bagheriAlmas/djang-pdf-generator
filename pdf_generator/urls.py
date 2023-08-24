f90fd98ad55c86c9ad5e791a72d967370a0d805d
mahdibagheri <bagheri.almas@gmail.com>
2023-08-24 14:50:15 +0330

    Added URL path for home.html in urls.py

--- pdf_generator/urls.py
+++ pdf_generator/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from pdf_generator.views import home_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
     path('', include('users.urls'))
 ]
