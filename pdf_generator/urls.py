0fc268437b46eae1286dd111e52a46558083db6a
mahdibagheri <bagheri.almas@gmail.com>
2023-08-24 20:06:24 +0330

    Moved views to notes app, removed redundant views

--- pdf_generator/urls.py
+++ pdf_generator/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from pdf_generator.views import home_view
+# from pdf_generator.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', home_view, name='home'),
-    path('', include('users.urls'))
+    path('', include('users.urls')),
+    path('', include('notes.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
