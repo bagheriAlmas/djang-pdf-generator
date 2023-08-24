acaf93af1c5b1f578aa7c7285d1d0dcce0917421
mahdibagheri <bagheri.almas@gmail.com>
2023-08-24 13:55:29 +0330

    create CustomUser model - inherit AbstractUser - add [age, phone, gender] fields to users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+USER_GENDER = (
+    ('Male', 'Male'),
+    ('Female', 'Female')
+)
+
+
+class CustomUser(AbstractUser):
+    age = models.PositiveSmallIntegerField(null=True, blank=True)
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=USER_GENDER, default='male')
+
+    def __str__(self):
+        return self.username
