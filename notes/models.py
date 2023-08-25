c6574dc51661d83e032cb36b3a2d893d109d18dc
mahdibagheri <bagheri.almas@gmail.com>
2023-08-25 18:23:19 +0330

    Create Report Model and Define it in admin.py

--- notes/models.py
+++ notes/models.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.db import models
 from users.models import CustomUser
 
 
 class Note(models.Model):
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    author = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     body = models.TextField()
     thumbnail = models.ImageField(upload_to='thumbnails', default='default.png')
@@ -12,3 +13,15 @@ class Note(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Report(models.Model):
+    author = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='reports/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author}\'s Report - {self.created_at}'
+
+    def get_absolute_url(self):
+        return self.file
