3c1d3e61fb227e1e5e00fbc6d36a45779e307ffa
mahdibagheri <bagheri.almas@gmail.com>
2023-08-25 15:13:28 +0330

    Configured STATIC_ROOT in settings.py for production static files serving Included URL pattern to serve static files in main urls.py

--- pdf_generator/settings.py
+++ pdf_generator/settings.py
@@ -97,11 +97,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -116,3 +111,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
