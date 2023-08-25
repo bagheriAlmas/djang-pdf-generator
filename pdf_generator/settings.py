ba68f1b83dd5985fee8d8035dbef6b4a849d419d
mahdibagheri <bagheri.almas@gmail.com>
2023-08-25 14:28:10 +0330

    fix mistake : fix LOGOUT_REDIRECT_URL path

--- pdf_generator/settings.py
+++ pdf_generator/settings.py
@@ -110,7 +110,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = '../../../'
-LOGOUT_REDIRECT_URL = 'registration/login'
+LOGOUT_REDIRECT_URL = '/accounts/login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
