7492b2cabde40974713a1a1ca47ddb6e8b888922
mahdibagheri <bagheri.almas@gmail.com>
2023-08-28 17:24:16 +0330

    Create tasks.py and implement Celery tasks for generating user reports

---
+++ notes/tasks.py
@@ -0,0 +1,29 @@
+import time
+from datetime import datetime
+from notes.models import Report, Note
+from pdf_generator.celery import app
+from users.models import CustomUser
+from .report_utils import generate_pdf_report
+
+
+@app.task
+def generate_report_data_for_user(user_id: int):
+    username = CustomUser.objects.only('username').get(pk=user_id).username
+    notes = Note.objects.filter(author_id=user_id)
+    time.sleep(10)
+    context = {
+        'notes': notes
+    }
+
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    file_name = f'{username} - Reports - {timestamp}'
+
+    try:
+        pdf_url = generate_pdf_report(context, 'reports/pdf_template.html', file_name)
+    except Exception as ex:
+        raise Exception(f'user : {user_id} - {ex}')
+
+    Report.objects.create(
+        author_id=user_id,
+        file=pdf_url
+    )
