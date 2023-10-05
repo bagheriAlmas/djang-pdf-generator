2b782a9b7a78e35755482c499e8848a2d0a478c1
mahdibagheri <bagheri.almas@gmail.com>
2023-10-05 12:45:48 +0330

    Add list_display fot Note and Report Model in admin panel

--- notes/admin.py
+++ notes/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Note, Report
 
-admin.site.register(Note)
-admin.site.register(Report)
+
+# admin.site.register(Note)
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'author', 'title', 'created_at']
+
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'author', 'file', 'read', 'created_at']
